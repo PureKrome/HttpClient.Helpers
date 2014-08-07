f31279707007abaf58dd14438130bc5a9cea5a58
Pure Krome <pure.krome@world-domination.com.au>
2014-08-08 08:46:22 +1000

    Renamed the default namespace.

--- Code/HttpClient.Helpers.Tests/FakeMessageHandlerFacts.cs
+++ Code/HttpClient.Helpers.Tests/FakeMessageHandlerFacts.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Shouldly;
+using WorldDomination.Net.Http;
 using Xunit;
 
 namespace WorldDomination.HttpClient.Helpers.Tests
--- Code/HttpClient.Helpers/HttpClientFactory.cs
+++ Code/HttpClient.Helpers/HttpClientFactory.cs
@@ -1,16 +1,16 @@
 ﻿using System.Net.Http;
 
-namespace WorldDomination.HttpClient.Helpers
+namespace WorldDomination.Net.Http
 {
     public static class HttpClientFactory
     {
         public static HttpMessageHandler MessageHandler { get; set; }
 
-        public static System.Net.Http.HttpClient GetHttpClient()
+        public static HttpClient GetHttpClient()
         {
             return MessageHandler == null
-                ? new System.Net.Http.HttpClient()
-                : new System.Net.Http.HttpClient(MessageHandler);
+                ? new HttpClient()
+                : new HttpClient(MessageHandler);
         }
     }
 }
