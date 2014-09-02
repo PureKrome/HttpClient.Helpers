c5b9515fad3c264353dbb5a39e673d6eebeebafc
Pure Krome <pure.krome@world-domination.com.au>
2014-09-02 10:55:03 +1000

    Added HttpClientFactory unit tests.

--- Code/HttpClient.Helpers.Tests/FakeMessageHandlerFacts.cs
+++ Code/HttpClient.Helpers.Tests/FakeMessageHandlerFacts.cs
@@ -20,16 +20,44 @@ namespace WorldDomination.HttpClient.Helpers.Tests
                 const string responseData = "I am not some Html.";
                 var messageResponse = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData);
                 var messageHandler = new FakeHttpMessageHandler(requestUrl, messageResponse);
-                var httpClient = new System.Net.Http.HttpClient(messageHandler);
 
-                // Act.
-                var result = await httpClient.GetAsync(requestUrl);
+                HttpResponseMessage message;
+                string content;
+                using (var httpClient = new System.Net.Http.HttpClient(messageHandler))
+                {
+                    // Act.
+                    message = await httpClient.GetAsync(requestUrl);
+                    content = await message.Content.ReadAsStringAsync();
+                }
 
                 // Assert.
-                result.StatusCode.ShouldBe(HttpStatusCode.OK);
-                var content = await result.Content.ReadAsStringAsync();
+                message.StatusCode.ShouldBe(HttpStatusCode.OK);
                 content.ShouldBe(responseData);
             }
+
+            [Fact]
+            public async Task GivenAnHttpResponseMessageAndTheHttpClientFactory_GetAsync_ReturnsAFakeResponse()
+            {
+                // Arrange.
+                const string requestUrl = "http://www.something.com/some/website";
+                const string responseData = "I am not some Html.";
+                var messageResponse = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData);
+                HttpClientFactory.MessageHandler = new FakeHttpMessageHandler(requestUrl, messageResponse);
+
+                HttpResponseMessage message;
+                string content;
+                using (var httpClient = HttpClientFactory.GetHttpClient())
+                {
+                    // Act.
+                    message = await httpClient.GetAsync(requestUrl);
+                    content = await message.Content.ReadAsStringAsync();
+                }
+
+                // Assert.
+                message.StatusCode.ShouldBe(HttpStatusCode.OK);
+                content.ShouldBe(responseData);
+            }
+
 
             [Fact]
             public async Task GivenAFewHttpResponseMessages_GetAsync_ReturnsAFakeResponse()
@@ -55,14 +83,57 @@ namespace WorldDomination.HttpClient.Helpers.Tests
                 };
 
                 var messageHandler = new FakeHttpMessageHandler(messageResponses);
-                var httpClient = new System.Net.Http.HttpClient(messageHandler);
-
-                // Act.
-                var result = await httpClient.GetAsync(requestUrl2);
+                
+                HttpResponseMessage message;
+                string content;
+                using (var httpClient = new System.Net.Http.HttpClient(messageHandler))
+                {
+                    // Act.
+                    message = await httpClient.GetAsync(requestUrl2);
+                    content = await message.Content.ReadAsStringAsync();
+                }
 
                 // Assert.
-                result.StatusCode.ShouldBe(HttpStatusCode.OK);
-                var content = await result.Content.ReadAsStringAsync();
+                message.StatusCode.ShouldBe(HttpStatusCode.OK);
+                content.ShouldBe(responseData2);
+            }
+
+            [Fact]
+            public async Task GivenAFewHttpResponseMessagesAndTheHttpClientFactory_GetAsync_ReturnsAFakeResponse()
+            {
+                // Arrange.
+                const string requestUrl1 = "http://www.something.com/some/website";
+                const string responseData1 = "I am not some Html.";
+                var messageResponse1 = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData1);
+
+                const string requestUrl2 = "http://www.something.com/another/site";
+                const string responseData2 = "Html, I am not.";
+                var messageResponse2 = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData2);
+
+                const string requestUrl3 = "http://www.whatever.com/";
+                const string responseData3 = "<html><head><body>pew pew</body></head>";
+                var messageResponse3 = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData3);
+
+                var messageResponses = new Dictionary<string, HttpResponseMessage>
+                {
+                    {requestUrl1, messageResponse1},
+                    {requestUrl2, messageResponse2},
+                    {requestUrl3, messageResponse3}
+                };
+
+                HttpClientFactory.MessageHandler = new FakeHttpMessageHandler(messageResponses);
+
+                HttpResponseMessage message;
+                string content;
+                using (var httpClient = HttpClientFactory.GetHttpClient())
+                {
+                    // Act.
+                    message = await httpClient.GetAsync(requestUrl2);
+                    content = await message.Content.ReadAsStringAsync();
+                }
+
+                // Assert.
+                message.StatusCode.ShouldBe(HttpStatusCode.OK);
                 content.ShouldBe(responseData2);
             }
 
@@ -75,14 +146,42 @@ namespace WorldDomination.HttpClient.Helpers.Tests
 
                 // No RequestUri == a wildcard == any url given.
                 var messageHandler = new FakeHttpMessageHandler(messageResponse);
-                var httpClient = new System.Net.Http.HttpClient(messageHandler);
 
-                // Act.
-                var result = await httpClient.GetAsync("http://pewpew.com");
+                HttpResponseMessage message;
+                string content;
+                using (var httpClient = new System.Net.Http.HttpClient(messageHandler))
+                {
+                    // Act.
+                    message = await httpClient.GetAsync("http://pewpew.com");
+                    content = await message.Content.ReadAsStringAsync();
+                }
 
                 // Assert.
-                result.StatusCode.ShouldBe(HttpStatusCode.OK);
-                var content = await result.Content.ReadAsStringAsync();
+                message.StatusCode.ShouldBe(HttpStatusCode.OK);
+                content.ShouldBe(responseData);
+            }
+
+            [Fact]
+            public async Task GivenAnHttpResponseMessageAndNoRequestUriAndTheHttpClientFactory_GetAsync_ReturnsAFakeResponse()
+            {
+                // Arrange.
+                const string responseData = "I am not some Html.";
+                var messageResponse = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData);
+
+                // No RequestUri == a wildcard == any url given.
+                HttpClientFactory.MessageHandler = new FakeHttpMessageHandler(messageResponse);
+
+                HttpResponseMessage message;
+                string content;
+                using (var httpClient = HttpClientFactory.GetHttpClient())
+                {
+                    // Act.
+                    message = await httpClient.GetAsync("http://pewpew.com");
+                    content = await message.Content.ReadAsStringAsync();
+                }
+
+                // Assert.
+                message.StatusCode.ShouldBe(HttpStatusCode.OK);
                 content.ShouldBe(responseData);
             }
 
@@ -110,14 +209,57 @@ namespace WorldDomination.HttpClient.Helpers.Tests
                 };
 
                 var messageHandler = new FakeHttpMessageHandler(messageResponses);
-                var httpClient = new System.Net.Http.HttpClient(messageHandler);
 
-                // Act.
-                var result = await httpClient.GetAsync("http://pewpew.com");
+                HttpResponseMessage message;
+                string content;
+                using (var httpClient = new System.Net.Http.HttpClient(messageHandler))
+                {
+                    // Act.
+                    message = await httpClient.GetAsync("http://pewpew.com");
+                    content = await message.Content.ReadAsStringAsync();
+                }
 
                 // Assert.
-                result.StatusCode.ShouldBe(HttpStatusCode.OK);
-                var content = await result.Content.ReadAsStringAsync();
+                message.StatusCode.ShouldBe(HttpStatusCode.OK);
+                content.ShouldBe(responseData3);
+            }
+
+            [Fact]
+            public async Task GivenAFewHttpResponseMessagesWithAWildcardAndTheHttpClientFactory_GetAsync_ReturnsTheWildcardResponse()
+            {
+                // Arrange.
+                const string requestUrl1 = "http://www.something.com/some/website";
+                const string responseData1 = "I am not some Html.";
+                var messageResponse1 = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData1);
+
+                const string requestUrl2 = "http://www.something.com/another/site";
+                const string responseData2 = "Html, I am not.";
+                var messageResponse2 = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData2);
+
+                const string requestUrl3 = "*";
+                const string responseData3 = "<html><head><body>pew pew</body></head>";
+                var messageResponse3 = FakeHttpMessageHandler.GetStringHttpResponseMessage(responseData3);
+
+                var messageResponses = new Dictionary<string, HttpResponseMessage>
+                {
+                    {requestUrl1, messageResponse1},
+                    {requestUrl2, messageResponse2},
+                    {requestUrl3, messageResponse3}
+                };
+
+                HttpClientFactory.MessageHandler = new FakeHttpMessageHandler(messageResponses);
+
+                HttpResponseMessage message;
+                string content;
+                using (var httpClient = HttpClientFactory.GetHttpClient())
+                {
+                    // Act.
+                    message = await httpClient.GetAsync("http://pewpew.com");
+                    content = await message.Content.ReadAsStringAsync();
+                }
+
+                // Assert.
+                message.StatusCode.ShouldBe(HttpStatusCode.OK);
                 content.ShouldBe(responseData3);
             }
 
@@ -128,12 +270,35 @@ namespace WorldDomination.HttpClient.Helpers.Tests
                 const string errorMessage = "Oh man - something bad happened.";
                 var exception = new HttpRequestException(errorMessage);
                 var messageHandler = new FakeHttpMessageHandler(exception);
-                var httpClient = new System.Net.Http.HttpClient(messageHandler);
 
-                // Act.
-                var result =
-                    Should.Throw<HttpRequestException>(
-                        async () => await httpClient.GetAsync("http://www.something.com/some/website"));
+                HttpRequestException result;
+                using (var httpClient = new System.Net.Http.HttpClient(messageHandler))
+                {
+                    // Act.
+                    result =
+                        Should.Throw<HttpRequestException>(
+                            async () => await httpClient.GetAsync("http://www.something.com/some/website"));
+                }
+
+                // Assert.
+                result.Message.ShouldBe(errorMessage);
+            }
+
+            [Fact]
+            public void GivenAnHttpClientExceptionAndTheHttpClientFactory_GetAsync_ReturnsAFakeResponse()
+            {
+                // Arrange.
+                const string errorMessage = "Oh man - something bad happened.";
+                var exception = new HttpRequestException(errorMessage);
+                HttpClientFactory.MessageHandler = new FakeHttpMessageHandler(exception);
+
+                HttpRequestException result;
+                using (var httpClient = HttpClientFactory.GetHttpClient())
+                {
+                    // Act.
+                    result = Should.Throw<HttpRequestException>(
+                            async () => await httpClient.GetAsync("http://www.something.com/some/website"));
+                }
 
                 // Assert.
                 result.Message.ShouldBe(errorMessage);
