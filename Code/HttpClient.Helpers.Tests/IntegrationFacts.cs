0a86e084bd9ac79f0349ce09a08724c895610c78
Pure Krome <github@world-domination.com.au>
2015-01-31 08:51:34 +1100

    Added an integration test.

---
+++ Code/HttpClient.Helpers.Tests/IntegrationFacts.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shouldly;
+using WorldDomination.Net.Http;
+using Xunit;
+
+namespace WorldDomination.HttpClient.Helpers.Tests
+{
+    public class IntegrationFacts
+    {
+        // This test checks to see if a normal integration test succeeds/fails.
+        [Fact]
+        public async Task GivenAGetRequest_GetStringAsync_ReturnsSomeData()
+        {
+            // Arrange.
+            string html;
+
+            HttpClientFactory.MessageHandler = new HttpClientHandler
+            {
+                Credentials = new NetworkCredential("api", "hi there!")
+            };
+
+            using (var httpClient = HttpClientFactory.GetHttpClient())
+            {
+                html = await httpClient.GetStringAsync("http://www.google.com.au");
+            }
+
+            // Assert.
+            html.ShouldNotBeNullOrEmpty();
+        }
+    }
+}
