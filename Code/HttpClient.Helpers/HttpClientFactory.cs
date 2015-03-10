4eaa4b42446421a231ad513ccd5db1e24112a376
Pure Krome <github@world-domination.com.au>
2015-03-10 14:53:03 +1100

    :bug: Static Message handler doesn't dispose after use. - When creating the new HttpClient and passing in the MessageHandler, it was getting disposed of after the first use. This was causing all types of grief. Now fixed :) - New optional parameter for GetHttpClient method. eg. Enables passing in network credentials per request, instead of a static global that is not thread safe.

--- Code/HttpClient.Helpers/HttpClientFactory.cs
+++ Code/HttpClient.Helpers/HttpClientFactory.cs
@@ -4,13 +4,28 @@ namespace WorldDomination.Net.Http
 {
     public static class HttpClientFactory
     {
+        /// <summary>
+        /// A custom message handler to use.
+        /// </summary>
+        /// <remarks>This is usually used for unit testing, so the HttpClient doesn't do a real request to some endpoint. Also, this is a static property so it's not thread safe.</remarks>
         public static HttpMessageHandler MessageHandler { get; set; }
 
-        public static HttpClient GetHttpClient()
+        /// <summary>
+        /// A normal HttpClient or one dependent upon a MessageHandler.
+        /// </summary>
+        /// <param name="httpMessageHandler">Optional message handler with specific rules for this client.</param>
+        /// <returns></returns>
+        public static HttpClient GetHttpClient(HttpMessageHandler httpMessageHandler = null)
         {
-            return MessageHandler == null
-                ? new HttpClient()
-                : new HttpClient(MessageHandler);
+            // Logic: The static Message Handler takes priority. Why? This is usually used for unit tests.
+            //        Also, we do not dispose of this message handler in case we reuse this again.
+            //        Next, we use any handler passed in.
+            //        Finally, no handler, just create a normal Http Client.
+            return MessageHandler != null
+                ? new HttpClient(MessageHandler, false)
+                : httpMessageHandler != null
+                    ? new HttpClient(httpMessageHandler)
+                    : new HttpClient();
         }
     }
 }
